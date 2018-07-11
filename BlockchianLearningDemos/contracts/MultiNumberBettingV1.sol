97a12744151afa21ff2536201d96f661d14cdbbb
_DevD <10431310+captPython@users.noreply.github.com>
2018-07-11 19:12:55 +0530

    commit
    commit

--- BlockchianLearningDemos/contracts/MultiNumberBettingV1.sol
+++ BlockchianLearningDemos/contracts/MultiNumberBettingV1.sol
@@ -2,9 +2,38 @@ pragma solidity ^0.4.22;
 
 
 contract MultiNumberBettingV1 {
-    int8 loserCount;
-    int8 winnerCount;
-    constructor(uint8 num1, uint8 num2, uint8 num3) {
 
+    uint loserCount;
+    uint winnerCount;
+    uint8[3] storageArray;
+
+    function guess(uint8 betNumber) public returns(bool) {
+      bool flag;
+
+     for(uint8 i; i < storageArray.length; i++){
+        if (storageArray[i] == betNumber) {
+          flag = true;
+          winnerCount++;
+          break;
+        } else {
+          flag = false;
+        }
+       
+     }
+     
+       if (flag = false){
+          loserCount++;
+       }
+      return flag;
+    } 
+
+    function totalGuesses() public view returns (uint){
+      return (winnerCount + loserCount);
     }
+
+     constructor(uint8 num1, uint8 num2, uint8 num3) public {
+        storageArray[0] = num1;   // 0
+        storageArray[1] = num2;   // 5
+        storageArray[2] = num3;   // 7
+    }   
 }
